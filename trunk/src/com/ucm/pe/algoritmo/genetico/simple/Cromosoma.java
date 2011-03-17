2201b20284680b9f7547ab0c52c1b947a5737665
ssjivan@gmail.com <ssjivan@gmail.com@6a1c2e5a-96ea-7404-397d-73bd3d747096>
2011-03-17 22:46:33 +0000

    Integrada la funcion 2, la 3 está en proceso, falla porque la suma de las puntuaciones sale negativa hay que ver qué pasa.

--- trunk/src/com/ucm/pe/algoritmo/genetico/simple/Cromosoma.java
+++ trunk/src/com/ucm/pe/algoritmo/genetico/simple/Cromosoma.java
@@ -16,7 +16,7 @@ public abstract class Cromosoma implements Cloneable{
 	public double puntuacion_individuo =0;
 	public double puntuacion_acumulada = 0;
 	public int longitud_cromosoma = 0;
-	public double fenotipo;
+	public double[] fenotipo;
 	
 	public Cromosoma() {
 		// TODO Auto-generated constructor stub
@@ -30,6 +30,7 @@ public abstract class Cromosoma implements Cloneable{
 		genes= new ArrayList<Gen>(numero_genes);
 		for (int i = 0; i < numero_genes; i++) {
 			genes.add(new Gen(valor_rango_min[i], valor_rango_max[i],precision));
+			
 		}
 		longitud_cromosoma=genes.get(0).getNum_caracteres_gen();
 		aptitud_cromosoma=evalua();
@@ -40,7 +41,7 @@ public abstract class Cromosoma implements Cloneable{
 	@Override
 	protected abstract Object clone() throws CloneNotSupportedException;
 	public abstract double evalua();
-	public abstract double fenotipo();
+	public abstract double[] fenotipo();
 	public abstract void evaluaRecalcula(int num_genes);
 	
 }
