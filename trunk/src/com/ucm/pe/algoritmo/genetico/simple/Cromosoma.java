36ceec02be41d1d23158f284fa4b3499ea671112
ssjivan@gmail.com <ssjivan@gmail.com@6a1c2e5a-96ea-7404-397d-73bd3d747096>
2011-03-17 00:44:14 +0000

    nuva versión comprobar los datos con el debug, desde la inicialización

---
+++ trunk/src/com/ucm/pe/algoritmo/genetico/simple/Cromosoma.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.ucm.pe.algoritmo.genetico.simple;
+
+import java.util.ArrayList;
+
+/**
+ * @author Ivan
+ *
+ */
+public abstract class Cromosoma implements Cloneable{
+	public int numero_genes = 0;
+	public ArrayList<Gen> genes;
+	public double aptitud_cromosoma = 0;
+	public double puntuacion_individuo =0;
+	public double puntuacion_acumulada = 0;
+	public int longitud_cromosoma = 0;
+	public double fenotipo;
+	
+	public Cromosoma() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Cromosoma(int num_genes, double[] valor_rango_min, double[] valor_rango_max , double precision){
+		numero_genes= num_genes;
+		puntuacion_acumulada=0;
+		puntuacion_individuo=0;
+		longitud_cromosoma=0;
+		genes= new ArrayList<Gen>(numero_genes);
+		for (int i = 0; i < numero_genes; i++) {
+			genes.add(new Gen(valor_rango_min[i], valor_rango_max[i],precision));
+			longitud_cromosoma++;
+		}
+		aptitud_cromosoma=evalua();
+		fenotipo=fenotipo();
+	}
+	
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		//de momento lo dejamos así, no sé si se necesitará algo más.
+		return super.clone();
+	}
+
+	public abstract double evalua();
+	public abstract double fenotipo();
+	public abstract void evaluaRecalcula(int num_genes);
+	
+}
---
+++ trunk/src/com/ucm/pe/problemas/funciones/CromosomaF1.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package com.ucm.pe.problemas.funciones;
+
+import com.ucm.pe.algoritmo.genetico.simple.Cromosoma;
+
+/**
+ * @author Ivan
+ *
+ */
+public class CromosomaF1 extends Cromosoma{
+	private double evaluacion;
+	private double fenotipo;
+	
+	public CromosomaF1(int num_genes, double[] valor_rango_min, double[] valor_rango_max , double precision) {
+		super(num_genes, valor_rango_min, valor_rango_max, precision);
+		evaluacion = 0;
+		fenotipo = genes.get(num_genes-1).getFenotipo();
+		double e = (double)Math.E;
+		double pi = (double)Math.PI;
+		evaluacion = 20 + e - 20.0 * Math.pow(e,-0.2*Math.abs(fenotipo)) -Math.pow(e, Math.cos(2.0*pi*fenotipo));
+	}
+
+	@Override
+	public double evalua() {
+		// TODO Auto-generated method stub
+		return evaluacion;
+	}
+
+	@Override
+	public double fenotipo() {
+		// TODO Auto-generated method stub
+		return fenotipo;
+	}
+
+	@Override
+	public void evaluaRecalcula(int num_genes) {
+		evaluacion = 0;
+		fenotipo = genes.get(num_genes-1).getFenotipo();
+		double e = (double)Math.E;
+		double pi = (double)Math.PI;
+		evaluacion = 20 + e - 20.0 * Math.pow(e,-0.2*Math.abs(fenotipo)) -Math.pow(e, Math.cos(2.0*pi*fenotipo));
+	}
+	
+}
